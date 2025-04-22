46283c9bb25de9a17ebef72288d53667ac5a4135
Ace <PC@HONGCF>
2025-04-22 22:27:42 +0800

    完善正则

--- reg/js/index.js
+++ reg/js/index.js
@@ -1,27 +1,23 @@
-
 $(function () {
     var jrt = jch.jRegexTester;
-    var $tbReg: JQuery = $("#tbReg");
-    var $tbInput: JQuery = $("#tbInput");
-    var $tbReplace: JQuery = $("#tbReplace");
-    var $tbOutput: JQuery = $("#tbOutput");
-    var $copyLink: JQuery = $('#copyLink');
-
-    var cmReg: CodeMirror.EditorFromTextArea = null;
-    var cmInput: CodeMirror.EditorFromTextArea = null;
-    var cmReplace: CodeMirror.EditorFromTextArea = null;
-    var cmOutput: CodeMirror.EditorFromTextArea = null;
-
-    var workMode: jch.jRegexTester.workMode = jrt.workMode.match;
-    var replaceMode: jch.jRegexTester.replaceMode = jrt.replaceMode.includeNoMatch;
-    var newLineChar: jch.jRegexTester.newLineChar = jrt.newLineChar.CRLF;
-    var RegExpOption: string = "g";
-
+    var $tbReg = $("#tbReg");
+    var $tbInput = $("#tbInput");
+    var $tbReplace = $("#tbReplace");
+    var $tbOutput = $("#tbOutput");
+    var $copyLink = $('#copyLink');
+    var cmReg = null;
+    var cmInput = null;
+    var cmReplace = null;
+    var cmOutput = null;
+    var workMode = jrt.workMode.match;
+    var replaceMode = jrt.replaceMode.includeNoMatch;
+    var newLineChar = jrt.newLineChar.CRLF;
+    var RegExpOption = "g";
     init();
-    function init(): void {
+    function init() {
         bind();
         changeWorkMode(jrt.workMode.match);
-        let queryObj = jch.Core.getQueryObject<{ reg: string, input: string, replace: string }>();
+        var queryObj = jch.Core.getQueryObject();
         if (jch.Core.isString(queryObj.reg)) {
             $tbReg.val(decodeURIComponent(queryObj.reg));
         }
@@ -34,51 +30,40 @@ $(function () {
         // $tbReg.val("([^?&=]+)=([^?&=]*)");
         // $tbInput.val("https://www.google.com.hk/search?q=CodeMirror&aqs=chrome&sourceid=chrome&es_sm=91&ie=UTF-8");
         // $tbReplace.val("\"$1\":\"$2\",\r\n");
-
         // cmReg = CodeMirror.fromTextArea(<HTMLTextAreaElement>$tbReg.get(0), {
-
         //     mode: "javascript",
-
         // });
         // cmReg.setValue("function xx(a){return 0;}");
         // cmInput = CodeMirror.fromTextArea(<HTMLTextAreaElement>$tbInput.get(0));
         // cmReplace = CodeMirror.fromTextArea(<HTMLTextAreaElement>$tbReplace.get(0));
         // cmOutput = CodeMirror.fromTextArea(<HTMLTextAreaElement>$tbOutput.get(0));
-
-
         //-----
         run();
     }
-
-    function bind(): void {
+    function bind() {
         //输入自动运行
         $tbReg.add($tbInput).add($tbReplace).bind("change input", function (e) {
             run();
         });
-
         //手动运行
         $("#btnRun").click(function (e) {
             run();
         });
-
         //修改工作模式
         $("input[name='workMode']").change(function (e) {
             changeWorkMode($("input[name='workMode']:checked").val());
             run();
         });
-
         //修改替换模式
         $("input[name='replaceMode']").change(function (e) {
             replaceMode = Number($("input[name='replaceMode']:checked").val());
             run();
         });
-
         //修改换行符
         $("input[name='newLineChar']").change(function (e) {
             newLineChar = Number($("input[name='newLineChar']:checked").val());
             run();
         });
-
         //修改正则选项
         $("input[name='RegExpOption']").change(function (e) {
             var $ipt = $(this);
@@ -87,44 +72,39 @@ $(function () {
             RegExpOption += $ipt.prop("checked") ? val : "";
             run();
         });
-
         //将输出内容导入输入内容框
         $("#btnOutputToInput").click(function (e) {
             $tbInput.val($tbOutput.val());
             run();
         });
-
         //将输入字符串调用decodeURIComponent解码
         $("#btnDecodeURI").click(function (e) {
             $tbInput.val(decodeURIComponent($tbInput.val()));
         });
-
         //将输入字符串当做base64进行解码
         $("#btnDecodeBase64").click(function (e) {
             // window.btoa();// base64编码
             try {
                 $tbInput.val(window.atob($tbInput.val()));
-            } catch (e) {
+            }
+            catch (e) {
                 alert('当前不是一个合法base64字符串');
             }
         });
-
         $copyLink.click(function () {
             var reg = encodeURIComponent($tbReg.val());
             var replace = encodeURIComponent($tbReplace.val());
-            var str = `${location.origin + location.pathname}?reg=${reg}&replace=${replace}`;
+            var str = location.origin + location.pathname + "?reg=" + reg + "&replace=" + replace;
             jch.Core.copyToClipboard(str);
-        })
+        });
     }
-
-    function run(): void {
-        var txtReg: string = $tbReg.val();
-        var txtInput: string = $tbInput.val();
-        var txtReplace: string = $tbReplace.val();
-        var reg: RegExp = valiInputReg($tbReg, RegExpOption);
-        var outputResult: string = "";
-        var matchArr: RegExpExecArray[] = [];
-
+    function run() {
+        var txtReg = $tbReg.val();
+        var txtInput = $tbInput.val();
+        var txtReplace = $tbReplace.val();
+        var reg = valiInputReg($tbReg, RegExpOption);
+        var outputResult = "";
+        var matchArr = [];
         if (txtReg.length == 0 || txtInput.length == 0) {
             return;
         }
@@ -136,45 +116,47 @@ $(function () {
             if (matchArr.length == 0) {
                 return valiOutput(false);
             }
-            $.each(matchArr, function (i: number, match: RegExpExecArray) {
+            $.each(matchArr, function (i, match) {
                 if (!(match instanceof Array)) {
                     return true;
                 }
                 outputResult += match[0] + (i == matchArr.length - 1 ? "" : jrt.getNewLineChar(newLineChar));
             });
-        } else if (workMode === jrt.workMode.replace) {
+        }
+        else if (workMode === jrt.workMode.replace) {
             outputResult = jch.jRegexTester.replace(reg, txtInput, txtReplace, replaceMode);
         }
         valiOutput(outputResult);
     }
-
     /**
      * 变更工作模式
      * @param  {jch.jRegexTester.workMode} wm 目标工作模式
      * @returns void
      */
-    function changeWorkMode(wm: jch.jRegexTester.workMode): void {
+    function changeWorkMode(wm) {
         workMode = Number(wm);
         var $row = $tbReplace.parents(".row");
         if (workMode === jrt.workMode.match) {
             $row.hide(0);
-        } else if (workMode === jrt.workMode.replace) {
+        }
+        else if (workMode === jrt.workMode.replace) {
             $row.show(0);
         }
     }
-
     /**
      * 校验正则表达式输入框输入的正则是否正确，返回正则对象
-     * 
+     *
      * @param {JQuery} $textarea 文本框的DOM对象
      * @param {string} regOption 正则表达式选项
      * @param {string} parentSelector 增加样式的父级选择器
      * @returns {RegExp} (description)
      */
-    function valiInputReg($textarea: JQuery, regOption: string = "", parentSelector: string = ".form-group"): RegExp {
-        var txt: string = $textarea.val();
-        var $form: JQuery = $textarea.parents(parentSelector).eq(0);
-        var reg: RegExp = null;
+    function valiInputReg($textarea, regOption, parentSelector) {
+        if (regOption === void 0) { regOption = ""; }
+        if (parentSelector === void 0) { parentSelector = ".form-group"; }
+        var txt = $textarea.val();
+        var $form = $textarea.parents(parentSelector).eq(0);
+        var reg = null;
         if (txt.length == 0) {
             return reg;
         }
@@ -187,10 +169,8 @@ $(function () {
         }
         return reg;
     }
-
-    function valiOutput(outputText: string | boolean) {
+    function valiOutput(outputText) {
         var $parent = $tbOutput.parents(".form-group").eq(0);
-
         if (typeof outputText === "boolean" && outputText === false) {
             $tbOutput.val("");
             $parent.addClass("has-error");
@@ -200,5 +180,4 @@ $(function () {
             $parent.removeClass("has-error");
         }
     }
-
 });
--- reg/js/jch.jRegexTester.js
+++ reg/js/jch.jRegexTester.js
@@ -1,20 +1,21 @@
-namespace jch {
-    const core = jch.Core;
-    export module jRegexTester {
-
+var jch;
+(function (jch) {
+    var core = jch.Core;
+    var jRegexTester;
+    (function (jRegexTester) {
         /**
          * 返回所有匹配结果
-         * 
+         *
          * @export
          * @param {RegExp} reg 正则表达式
          * @param {string} input 输入字符串
          * @returns {RegExpExecArray[]} 返回所有结果
          */
-        export function matchs(reg: RegExp, input: string = ""): RegExpExecArray[] {
-            var result: RegExpExecArray[] = [];
-            var execArr: RegExpExecArray = null;
+        function matchs(reg, input) {
+            if (input === void 0) { input = ""; }
+            var result = [];
+            var execArr = null;
             reg.lastIndex = 0;
-
             if (!(reg instanceof RegExp)) {
                 throw new Error("Not RegExp");
             }
@@ -29,15 +30,14 @@ namespace jch {
                         break;
                     }
                 }
-            } else {
+            }
+            else {
                 result.push(reg.exec(input));
             }
-
             return result;
-
-            function checkNullArray(arr: string[]) {
-                var isNull: boolean = true;
-                $.each(arr, function(i: number, s: string) {
+            function checkNullArray(arr) {
+                var isNull = true;
+                $.each(arr, function (i, s) {
                     if (s != null && s.length > 0) {
                         isNull = false;
                         return false;
@@ -46,11 +46,10 @@ namespace jch {
                 return isNull;
             }
         }
-
-
+        jRegexTester.matchs = matchs;
         /**
          * 替换字符串
-         * 
+         *
          * @export
          * @param {RegExp} reg 正则表达式
          * @param {string=""} input 输入字符串
@@ -58,41 +57,45 @@ namespace jch {
          * @param {replaceMode} replacemode 替换模式
          * @returns {string} 返回替换后字符串
          */
-        export function replace(reg: RegExp, input: string = "", replaceStr: string = "", replacemode: replaceMode = replaceMode.includeNoMatch): string {
-            var result: string = "";
-
-            if (!(reg instanceof RegExp)) throw new Error("Not RegExp");
-            if (input.length == 0) return result;
-
+        function replace(reg, input, replaceStr, replacemode) {
+            if (input === void 0) { input = ""; }
+            if (replaceStr === void 0) { replaceStr = ""; }
+            if (replacemode === void 0) { replacemode = replaceMode.includeNoMatch; }
+            var result = "";
+            if (!(reg instanceof RegExp))
+                throw new Error("Not RegExp");
+            if (input.length == 0)
+                return result;
             if (replacemode === replaceMode.includeNoMatch) {
                 return input.replace(reg, replaceStr);
             }
             else if (replacemode === replaceMode.excludeNoMatch) {
-                input.replace(reg, (matchStr: string, ...args: any[]) => {
+                input.replace(reg, function (matchStr) {
+                    var args = [];
+                    for (var _i = 1; _i < arguments.length; _i++) {
+                        args[_i - 1] = arguments[_i];
+                    }
                     //若正则为 .(.)(.) ，则函数参数1为整个匹配到的三个字符串，参数2为分组1内容，参数3为分组2内容，参数4为起始位置，参数5为源字符串
                     //若正则为 . ，则函数参数1为整个匹配到的三个字符串，参数4为起始位置，参数5为源字符串
                     //换言之，至少有3个参数，参数1是固定的，为匹配到的字符串，参数倒数第1是源字符串，参数倒数第二位匹配位置，其余中间是分组内容
-                    var groups: string[] = args.slice(0, args.length - 2);
+                    var groups = args.slice(0, args.length - 2);
                     var index = args[args.length - 2];
                     var src = args[args.length - 1];
                     result += matchStr.replace(reg, replaceStr);
                     // console.log(result);
-
                     return "";
                 });
                 return result;
             }
         }
-
-
+        jRegexTester.replace = replace;
         //----------------------工具函数-------------------------------
-
         /**
          * 获取换行符枚举对应的换行符号
          * @param  {newLineChar} enumNewLine 换行符枚举
          * @returns string
          */
-        export function getNewLineChar(enumNewLine: newLineChar): string {
+        function getNewLineChar(enumNewLine) {
             switch (enumNewLine) {
                 case newLineChar.CRLF: {
                     return "\r\n";
@@ -108,82 +111,79 @@ namespace jch {
                 }
             }
         }
+        jRegexTester.getNewLineChar = getNewLineChar;
         //------------------------------枚举----------------------------
-
         /**
          * 正则替换模式
-         * 
+         *
          * @enum {number}
          */
-        export enum replaceMode {
+        var replaceMode;
+        (function (replaceMode) {
             /**
              * 替换后包含未匹配字符串（默认）
              */
-            includeNoMatch = 0,
+            replaceMode[replaceMode["includeNoMatch"] = 0] = "includeNoMatch";
             /**
              * 替换后不包含未匹配字符串
              */
-            excludeNoMatch
-        }
-
-
+            replaceMode[replaceMode["excludeNoMatch"] = 1] = "excludeNoMatch";
+        })(replaceMode = jRegexTester.replaceMode || (jRegexTester.replaceMode = {}));
         /**
          * jRegexTester的工作模式
-         * 
+         *
          * @enum {number}
          */
-        export enum workMode {
-
+        var workMode;
+        (function (workMode) {
             /**
              * 匹配查找模式
              */
-            match = 0,
+            workMode[workMode["match"] = 0] = "match";
             /**
              * 替换模式
              */
-            replace = 1
-        }
-
+            workMode[workMode["replace"] = 1] = "replace";
+        })(workMode = jRegexTester.workMode || (jRegexTester.workMode = {}));
         /**
          * 换行符枚举
-         * 
+         *
          * @enum {number}
          */
-        export enum newLineChar {
+        var newLineChar;
+        (function (newLineChar) {
             /**
              * 回车换行\r\n(windows)
              */
-            CRLF = 0,
+            newLineChar[newLineChar["CRLF"] = 0] = "CRLF";
             /**
              * 回车\r(Mac)
              */
-            CR,
+            newLineChar[newLineChar["CR"] = 1] = "CR";
             /**
              * 换行\n(Linux)
              */
-            LF
-        }
-
+            newLineChar[newLineChar["LF"] = 2] = "LF";
+        })(newLineChar = jRegexTester.newLineChar || (jRegexTester.newLineChar = {}));
         /**
          * 正则表达式选项
-         * 
+         *
          * @enum {number}
          */
-        export enum RegExpOption {
+        var RegExpOption;
+        (function (RegExpOption) {
             /**
              * 全局匹配
              */
-            global = 0,
-
+            RegExpOption[RegExpOption["global"] = 0] = "global";
             /**
              * 忽略大小写
              */
-            ignoreCase,
-
+            RegExpOption[RegExpOption["ignoreCase"] = 1] = "ignoreCase";
             /**
              * 多行模式
              */
-            multiline
-        }
-    }
-}
+            RegExpOption[RegExpOption["multiline"] = 2] = "multiline";
+        })(RegExpOption = jRegexTester.RegExpOption || (jRegexTester.RegExpOption = {}));
+    })(jRegexTester = jch.jRegexTester || (jch.jRegexTester = {}));
+})(jch || (jch = {}));
